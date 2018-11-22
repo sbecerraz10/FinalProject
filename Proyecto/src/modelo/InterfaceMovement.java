6964f4e4cf894828558a064ef09a059693b2ef52
KAMILO <KAMILO@172.30.163.237>
2018-11-22 16:52:34 -0500

    Camilo

--- Proyecto/src/modelo/InterfaceMovement.java
+++ Proyecto/src/modelo/InterfaceMovement.java
@@ -4,7 +4,7 @@ public interface InterfaceMovement {
 
 	public static final int LIMITX = 693;
 	
-	public static final int LIMITY = 243;
+	public static final int LIMITY = 360;
 	
 	public static final int MOVEMENT_RATING = 10;
 	
